5a96d344f2d37861b3ff55be6e187fcffbc81f97
Ronak Patel <ronak0624@gmail.com>
2020-03-03 19:46:13 -0800

    rest of previous changes, did not commit from home folder

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,1 +1,26 @@
-$(".")
+$(".carousel-item").on("click", function() {
+    let image = $(this).children("img").attr("src");
+    console.log(image)
+    openLightbox(image);
+});
+
+// $("#lightbox svg").on("click", function() {
+//     closeLightbox();
+// });
+
+$("#lightbox").on("click", function(event) {
+    if(event.target.tagName !== "IMG") {
+        closeLightbox();
+    }
+});
+
+// pre: Takes string for input to open
+// post: Opens lightbox with given image
+function openLightbox(source) {
+    $("#lightbox img").attr("src", source);
+    $("#lightbox").removeClass("d-none");
+}
+
+function closeLightbox() {
+    $("#lightbox").addClass("d-none");
+}
