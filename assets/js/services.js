22f3b462c027c2fcfa56476ac5b89362a6cfd38c
Ronak Patel <ronak0624@gmail.com>
2020-03-31 00:08:52 -0700

    services carousel

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,51 +1,36 @@
-var services = [
-    "Architectural Services",
-    "Notices & Agreements",
-    "Building Contractor",
-    "Project Management",
-    "Landscaping Services",
-]
-
-var current = 0;
-var next = 1;
-var prev = -1;
-
-$('.header-carousel span').on("click", function(e) {
-    let direction = e.target.className.split("my-auto ")[1]
-
-    if(direction === "next") {
-        if(next < services.length) {
-            prev = current;
-            current = next;
-            next++;
+$('.headers').slick({
+    arrows: true,
+    // centerMode: true,
+    infinite: false,
+    asNavFor: '.content',
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: true,
+            // centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: true,
+            // centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
         }
-    }
+      ]
+});
 
-    if(direction === "previous") {
-        if(prev >= 0) {
-            next = current;
-            current = prev;
-            prev--;
-        }
-    }
-
-    var previousHtml = $(".header-carousel .previous")
-    var currentHtml = $(".header-carousel h1")
-    var nextHtml = $(".header-carousel .next")
-
-    if(prev === -1) {
-        previousHtml.text("");
-    }
-
-    if(next >= services.length) {
-        nextHtml.text("")
-    }
-
-    if(next <= services.length && prev >= -1) {
-        previousHtml.text(services[prev])
-        currentHtml.text(services[current])
-        nextHtml.text(services[next])
-        console.log(services[prev], services[current], services[next])
-    }
-
+$('.content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.headers',
+    infinite: false,
+    arrows: false,
+    dots: true,
+    focusOnSelect: true
 });
