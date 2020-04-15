ff3572f9055f36a6c5b5a7f2e538ac8cf33e7943
Ronak Patel <ronak0624@gmail.com>
2020-04-15 02:41:55 -0700

    contact submission accepted page

--- pages/contact.php
+++ pages/contact.php
@@ -2,15 +2,13 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "ronak0624@gmail.com";
+    $email_to = "bexcresthomes@gmail.com";
     $email_subject = "New Inquiry for Bexcrest";
  
     function died($error) {
         // your error code can go here
         echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
         echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
         die();
     }
  
@@ -42,15 +40,15 @@ if(isset($_POST['email'])) {
     $string_exp = "/^[A-Za-z .'-]+$/";
  
   if(!preg_match($string_exp,$name)) {
-    $error_message .= 'The Name you entered does not appear to be valid.<br />';
-  }
- 
-  if(!preg_match($string_exp,$address)) {
-    $error_message .= 'The Address you entered does not appear to be valid.<br />';
+    $error_message .= 'The name you entered does not appear to be valid.<br />';
   }
  
   if(strlen($message) < 2) {
     $error_message .= 'The message you entered do not appear to be valid.<br />';
+  }
+
+  if(strlen($address) < 5) {
+    $error_message .= 'The address you entered does not appear to be valid.<br />';
   }
  
   if(strlen($error_message) > 0) {
@@ -81,8 +79,30 @@ $headers = 'From: '.$email."\r\n".
 ?>
  
 <!-- include your own success html here -->
- 
-Thank you for contacting us. We will be in touch with you very soon.
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Testimonials</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../assets/css/bexcrest.css">
+</head>
+
+<body>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Thank you for your inquiry.</h1>
+    <p class="lead">We will be in touch soon.</p>
+  </div>
+</div>
+</body>
  
 <?php
  
