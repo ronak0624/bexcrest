cc41ae69d78ebcba1432c2b50310d48851154cc4
Ronak Patel <ronak0624@gmail.com>
2020-04-19 02:17:29 -0700

    switched to better lightbox for gallery

---
+++ assets/js/portfolio.js
@@ -0,0 +1,331 @@
+const urls = {
+    "thornhill": [
+      {
+        "src": "../assets/img/portfolio/thornhill/front.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/thornhill/bathroom3.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/thornhill/ensuite5.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/thornhill/bedroom5.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/thornhill/kitchen-diner2.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/thornhill/bedroom6.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/thornhill/living-room.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/thornhill/rear-garden.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/thornhill/kitchen-diner.jpg",
+        "w": 1200,
+        "h": 900
+      }
+    ],
+    "kingston": [
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_3609.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_1249.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_3611.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_1247.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_1250.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_3612.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_2046.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_2637.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_2048.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_2638.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_2045.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_2633.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_1287.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_3606.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_3607.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/kingston/IMG_3608.jpg",
+        "w": 900,
+        "h": 1200
+      }
+    ],
+    "johnston": [
+      {
+        "src": "../assets/img/portfolio/johnston/JT1.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/johnston/JT2.jpg",
+        "w": 800,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/johnston/JT3.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/johnston/JT4.jpg",
+        "w": 900,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/johnston/JT5.jpg",
+        "w": 899,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/johnston/JT6.jpg",
+        "w": 899,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/johnston/JT7.jpg",
+        "w": 899,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/johnston/JT8.jpg",
+        "w": 899,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/johnston/JT9.jpg",
+        "w": 899,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/johnston/JT10.jpg",
+        "w": 1400,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/johnston/JT11.jpg",
+        "w": 899,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/johnston/JT12.jpg",
+        "w": 899,
+        "h": 1200
+      },
+      {
+        "src": "../assets/img/portfolio/johnston/JT13.jpg",
+        "w": 899,
+        "h": 1200
+      }
+    ],
+    "avenue": [
+      {
+        "src": "../assets/img/portfolio/avenue/AG1.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/avenue/AG2.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/avenue/AG3.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/avenue/AG4.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/avenue/AG5.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/avenue/AG6.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/avenue/AG7.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/avenue/AG8.jpg",
+        "w": 1200,
+        "h": 900
+      }
+    ],
+    "anderson": [
+      {
+        "src": "../assets/img/portfolio/anderson/AD%201.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/anderson/AD%202.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/anderson/AD%203.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/anderson/AD%204.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/anderson/AD%205.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/anderson/AD%206.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/anderson/AD%207.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/anderson/AD%208.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/anderson/AD%209.jpg",
+        "w": 1200,
+        "h": 900
+      }
+    ],
+    "rye": [
+      {
+        "src": "../assets/img/portfolio/rye/1.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/rye/2.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/rye/3.jpg",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/rye/4.jpg",
+        "w": 1200,
+        "h": 900
+      }
+    ],
+    "upcoming": [
+      {
+        "src": "../assets/img/portfolio/upcoming/surbiton-front.png",
+        "w": 1200,
+        "h": 900
+      },
+      {
+        "src": "../assets/img/portfolio/upcoming/surbiton-rear.png",
+        "w": 1200,
+        "h": 900
+      }
+    ]
+  }
+
+var pswpElement = document.querySelectorAll('.pswp')[0];
+
+$('button.preview').click(function(e) {
+    let selected = $(this).attr("data-property");
+    $(this).children("button.preview .overlay").addClass("d-none");
+
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, urls[selected]);
+    gallery.init();
+})
